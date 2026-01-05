0a5a6b84648bb0183a6f279679c117550f133526
LyHung <huntly1306@gmail.com>
2026-01-05 07:39:21 +0700

    Change shopify.php setup

--- config/shopify.php
+++ config/shopify.php
@@ -3,7 +3,7 @@ return [
     'shopify' => [
         'key' => env('SHOPIFY_API_KEY'),
         'secret' => env('SHOPIFY_API_SECRET'),
-        'scopes' => env('SHOPIFY_SCOPES', 'read_products,write_products'),
+        'scopes' => env('SHOPIFY_SCOPES', 'read_products,write_products,read_products,write_orders'),
         'api_version' => env('SHOPIFY_API_VERSION', '2025-10'),
     ],
 ];
