1a390d7b7294d6e1755c55027ba8b2bd4e67663b
LyHung <huntly1306@gmail.com>
2026-01-05 07:50:13 +0700

    Change defineConfig

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',   // cho phép truy cập từ localhost hoặc Docker
+        port: 5173,        // port cố định
+        cors: true,        // cho phép truy cập cross-origin
+        hmr: {
+            protocol: 'ws',  // WebSocket bình thường
+            host: 'localhost', // host trình duyệt sẽ connect
+            port: 5173,      // port Vite dev server
+        },
+        allowedHosts: ['localhost'],  // chỉ localhost
+    },
+
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: ['resources/js/app.jsx'],
+            refresh: true, // bật React Refresh
         }),
+        react(),
     ],
 });
